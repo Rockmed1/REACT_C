af0b0630a96d3b01d29dd1c2ab374c76cfa88842
Rockmed1 <186123210+Rockmed1@users.noreply.github.com>
2025-02-22 11:54:00 -0800

    Router starter

---
+++ 11-worldwise/worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div className="nav">
+      <AppNav />
+      <p>app</p>
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ 11-worldwise/worldwise/src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pricing() {
+  return (
+    <>
+      <PageNav />
+      <h2>Pricing</h2>
+    </>
+  );
+}
+
+export default Pricing;
